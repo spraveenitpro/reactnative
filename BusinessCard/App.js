f5bbc29d904f05bbf947319a77bf7c9abf94363d
Praveen <spraveenitpro@gmail.com>
2024-12-19 22:58:52 -0800

    Add icons to the project

--- BusinessCard/App.js
+++ BusinessCard/App.js
@@ -1,9 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, Button, Linking } from 'react-native';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome5';
 
 export default function App() {
-    return (
+
+    const onContactMe = () => {
+        console.warn('Contact Me!')
+        Linking.openURL("mailto:teste@gmail.com")
+    }
+
+return (
+<ScrollView>
         <View style={styles.container}>
 
             <Image
@@ -25,12 +32,40 @@ export default function App() {
             <Text style={{ fontSize: 25, fontWeight: 'bold' }}>React Native!</Text>
             <Text>Making React Native apps</Text>
             <View style={{flexDirection: 'row', marginVertical: 10, gap:10}}>
-                <FontAwesome6 name="github" size={24} color="black" />            <StatusBar style="auto" />
-                <FontAwesome6 name="twitter" size={24} color="black" />            <StatusBar style="auto" />
+                <FontAwesome6 name="github" size={24} color="black" />
+                <FontAwesome6 name="twitter" size={24} color="black" />
                 <FontAwesome6 name="at" size={24} color="black" />
             </View>
-            <StatusBar style="auto" />
+            <Button title="Contact me" onPress={onContactMe}/>
+
+            <Text style={{ padding: 10, fontSize: 16, lineHeight: 20 }}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam
+    aliquam sem et tortor consequat id porta nibh. Pellentesque nec
+    nam aliquam sem. Placerat duis ultricies lacus sed. Non curabitur
+    gravida arcu ac tortor dignissim convallis aenean. Amet nisl purus
+    in mollis nunc. Vel elit scelerisque mauris pellentesque pulvinar
+    pellentesque. Sagittis orci a scelerisque purus semper eget duis
+    at tellus. Sed libero enim sed faucibus turpis in eu mi bibendum.
+    Duis at consectetur lorem donec massa sapien faucibus et molestie.
+    At ultrices mi tempus imperdiet nulla malesuada pellentesque elit
+    eget. Purus sit amet volutpat consequat mauris nunc congue nisi
+    vitae. Urna condimentum mattis pellentesque id nibh tortor id.
+    Consequat id porta nibh venenatis. Lectus vestibulum mattis
+    ullamcorper velit sed ullamcorper. Mauris a diam maecenas sed enim
+    ut sem. Volutpat commodo sed egestas egestas fringilla phasellus.
+    Turpis egestas integer eget aliquet nibh praesent tristique magna
+    sit. Congue mauris rhoncus aenean vel elit scelerisque. Tellus
+    integer feugiat scelerisque varius morbi enim. Consectetur a erat
+    nam at. Bibendum arcu vitae elementum curabitur vitae nunc. Sit
+    amet consectetur adipiscing elit. Rhoncus mattis rhoncus urna
+    neque viverra justo. Malesuada pellentesque elit eget gravida.
+    Vitae nunc sed velit dignissim sodales ut eu sem integer.
+  </Text>
+
+
             </View>
+            </ScrollView>
     );
 }
 
