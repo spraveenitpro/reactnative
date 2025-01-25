71075037dfe0bee3ee202ebcea3865cb5e942b12
Praveen <spraveenitpro@gmail.com>
2025-01-24 21:05:55 -0800

    Implment Quiz screens

--- Quiz/src/app/QuizScreen.tsx
+++ Quiz/src/app/QuizScreen.tsx
@@ -1,10 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import QuestionCard from '../components/QuestionCard';
 
 export default function QuizScreen() {
     return (
         <View style={styles.container}>
+            {/* Header */}
+            <View>
+                <Text>Question 1/5</Text>
+            </View>
+
+            {/*Body */}
             <QuestionCard />
+
+            {/*Footer */}
+            <View>
+                <Text>Footer</Text>
+            </View>
         </View>
     )
 }
@@ -13,7 +24,7 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#FDFEF4',
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         padding: 20,
     }
 })
