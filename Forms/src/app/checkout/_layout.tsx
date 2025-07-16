e1afcede8ce3c5a010ebd808d5f2f6f8acc71c25
Praveen <spraveenitpro@gmail.com>
2025-07-15 22:58:12 -0700

    Add additional fields to the form

--- Forms/src/app/checkout/_layout.tsx
+++ Forms/src/app/checkout/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from 'expo-router';
 
 export default function CheckoutLayout() {
 	return (
-		<Stack>
+		<Stack screenOptions={{ headerShown: false }}>
 			<Stack.Screen name="personal" options={{ title: 'Personal!' }} />
 			<Stack.Screen name="payment" options={{ title: 'Payment' }} />
 			<Stack.Screen name="confirm" options={{ title: 'Confirm' }} />
--- Forms/src/app/checkout/personal.tsx
+++ Forms/src/app/checkout/personal.tsx
@@ -1,8 +1,17 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import {
+	View,
+	Text,
+	StyleSheet,
+	TextInput,
+	ScrollView,
+	KeyboardAvoidingView,
+	Platform,
+} from 'react-native';
 import { Link, router } from 'expo-router';
 import CustomButton from '../../components/CustomButton';
 import CustomTextInput from '../../components/CustomTextInput';
 import { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function PersonalDetailsForm() {
 	const [fullname, setFullname] = useState('');
@@ -12,44 +21,64 @@ export default function PersonalDetailsForm() {
 	};
 
 	return (
-		<View style={styles.container}>
-			<CustomTextInput label="Full name" placeholder="Joe dash" />
-			<CustomTextInput label="Address" placeholder="Address!" />
-			<View
-				style={{
-					flexDirection: 'row',
-					gap: 5,
-				}}
+		<KeyboardAvoidingView
+			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+			style={{ flex: 1 }}
+		>
+			<ScrollView
+				style={{ backgroundColor: 'white' }}
+				contentContainerStyle={styles.container}
+				keyboardShouldPersistTaps="handled"
 			>
-				<CustomTextInput
-					label="City"
-					placeholder="Bangalore"
-					containerStyle={{ flex: 1 }}
-				/>
-				<CustomTextInput
-					label="Post Code"
-					placeholder="560043"
-					containerStyle={{ flex: 1 }}
-				/>
-			</View>
-			<CustomTextInput
-				label="Phone number"
-				placeholder="2361231212"
-				inputMode="tel"
-			/>
-			<CustomButton title="Next" style={styles.button} onPress={onNext} />
-		</View>
+				<SafeAreaView edges={['bottom', 'top']}>
+					<CustomTextInput label="Full name" placeholder="Joe dash" />
+					<CustomTextInput label="Full name" placeholder="Joe dash" />
+					<CustomTextInput label="Full name" placeholder="Joe dash" />
+					<CustomTextInput label="Full name" placeholder="Joe dash" />
+					<CustomTextInput label="Full name" placeholder="Joe dash" />
+					<CustomTextInput label="Full name" placeholder="Joe dash" />
+					<CustomTextInput label="Full name" placeholder="Joe dash" />
+					<CustomTextInput label="Address" placeholder="Address!" />
+					<View
+						style={{
+							flexDirection: 'row',
+							gap: 5,
+						}}
+					>
+						<CustomTextInput
+							label="City"
+							placeholder="Bangalore"
+							containerStyle={{ flex: 1 }}
+						/>
+						<CustomTextInput
+							label="Post Code"
+							placeholder="560043"
+							containerStyle={{ flex: 1 }}
+						/>
+					</View>
+					<CustomTextInput
+						label="Phone number"
+						placeholder="2361231212"
+						inputMode="tel"
+					/>
+					<CustomButton
+						title="Next"
+						style={styles.button}
+						onPress={onNext}
+					/>
+				</SafeAreaView>
+			</ScrollView>
+		</KeyboardAvoidingView>
 	);
 }
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
+		flexGrow: 1,
 		backgroundColor: 'white',
 		padding: 10,
 		gap: 5,
 	},
 	button: {
 		marginTop: 'auto',
-		marginBottom: 25,
 	},
 });
